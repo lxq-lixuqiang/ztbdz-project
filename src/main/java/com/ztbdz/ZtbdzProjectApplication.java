64083d58c734ef5ef6888076401cddbe165f9181
lixuqiang3204 <HMV33T3D>
2025-07-15 17:12:37 +0800

    项目添加gzip进行传输压缩内容提高加载页面速度

--- src/main/java/com/ztbdz/ZtbdzProjectApplication.java
+++ src/main/java/com/ztbdz/ZtbdzProjectApplication.java
@@ -3,9 +3,11 @@ package com.ztbdz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("com.ztbdz.*.mapper")
+@EnableCaching
 public class ZtbdzProjectApplication {
 
     public static void main(String[] args) {
