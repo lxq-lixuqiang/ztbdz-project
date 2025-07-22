9cf281aecd6474ba14a1f0bb4e75e17f97955644
lixuqiang3204 <HMV33T3D>
2025-07-22 17:10:20 +0800

    优化前端配置信息整合设置配置并将配置信息进行加密处理

--- src/main/resources/static/config/token.js
+++ src/main/resources/static/config/token.js
@@ -22,6 +22,34 @@ function removeToken() {
     localStorage.removeItem('member');
 }
 
+// 配置信息 加密：btoa(unescape(encodeURIComponent("需要加密的内容")))
+var config = {
+    refundReason  : decoded("6K+35Y+R6YCB6YKu566x5YiwIOKAnDI4MTc4OTk3OTRxcS5jb23igJ3vvIzor7TmmI7pgIDotLnljp/lm6DlubbpmYTkuIog4oCc5pSv5LuY5oiQ5Yqf55qE5oiq5Zu+4oCdIOWSjOaIquWbvumhtemdouaciSDigJzmiqXlkI3kuI3pgJrov4figJ0g55qE6aG555uuIOi/m+ihjOmAgOi0ueWuoeaguO+8gQ=="), //....退费流程说明 "请发送邮箱到 “2817899794qq.com”，说明退费原因并附上 “支付成功的截图” 和截图页面有 “报名不通过” 的项目 进行退费审核！"
+    consultPhone : decoded("6aG555uu5ZKo6K+i55S16K+d77yaMDI4LTg2NzE2Mjg5"), //....咨询电话 "项目咨询电话：028-86716289"
+    skillPhone : decoded("5bmz5Y+w5oqA5pyv55S16K+d77yaMTM4LTgxMTItMzIwNA=="), //....技术电话 "平台技术电话：138-8112-3204"
+    pay : {
+        receivingAccount : decoded("5Zub5bed5Lit5aSp6Ziz5YWJ5oub5qCH5Luj55CG5pyJ6ZmQ5YWs5Y+4"), //....收款账户 "四川中天阳光招标代理有限公司"
+        bankAccountNumber : decoded("NjMxMCAwNTcxNg=="), //....银行账号 "6310 05716"
+        bankDeposit : decoded("5Lit5Zu95rCR55Sf6ZO26KGM5oiQ6YO96ZOB5YOP5a+65pSv6KGM"), //....开户银行 "中国民生银行成都铁像寺支行"
+        qrCode : decoded("aW1hZ2VzL3BheW1lbnQtcXIuanBn") //....微信付款二维码图片 "images/payment-qr.jpg"
+    }
+}
+
+// 正则表达式校验
+var regBox = {
+    regEmail : /^([a-zA-Z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,   //....邮箱："邮箱格式错误"
+    regName : /^[a-zA-Z0-9_-]{2,16}$/,                       //....用户名："用户名必须是2~16位的字母数字_-"
+    regPassword : /^[a-zA-Z0-9@$_-]{6,16}$/,                 //....密码："密码必须是6~16位的字母数字@$_-"
+    regIdCard : /^[1-9]([0-9]{13}|[0-9]{16})[0-9xX]$/,       //....身份证号："身份证号格式错误"
+    regMobilePhone : /^0?1[3|4|5|8][0-9]\d{8}$/,                 //....手机："手机号格式错误"
+    regTelephone : /^0[\d]{2,3}-[\d]{7,8}$/                     //....电话："电话号码格式错误"
+}
+
+// 解密
+function decoded(encryptionStr){
+    return decodeURIComponent(escape(atob(encryptionStr)));
+}
+
 // 校验token是否有效
 function verifyLogin(){
     if(!getToken()){
@@ -196,13 +224,4 @@ function pageShow(page){
     initPageShow(JSON.parse(localStorage.getItem('data')));
 }
 
-// 正则表达式校验
-var regBox = {
-    regEmail : /^([a-zA-Z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,   //....邮箱："邮箱格式错误"
-    regName : /^[a-zA-Z0-9_-]{2,16}$/,                       //....用户名："用户名必须是2~16位的字母数字_-"
-    regPassword : /^[a-zA-Z0-9@$_-]{6,16}$/,                 //....密码："密码必须是6~16位的字母数字@$_-"
-    regIdCard : /^[1-9]([0-9]{13}|[0-9]{16})[0-9xX]$/,       //....身份证号："身份证号格式错误"
-    regMobilePhone : /^0?1[3|4|5|8][0-9]\d{8}$/,                 //....手机："手机号格式错误"
-    regTelephone : /^0[\d]{2,3}-[\d]{7,8}$/                     //....电话："电话号码格式错误"
-}
 
