12e68e819f037502fe89c7989e96bcf057da942a
lixuqiang3204 <HMV33T3D>
2025-08-25 16:06:02 +0800

    调整联系我们按钮展示平台技术电话，项目咨询电话等信息

--- src/main/resources/static/js/contactusdiyou.js
+++ src/main/resources/static/js/contactusdiyou.js
@@ -33,33 +33,6 @@ $(function() {
         '\t\t\t\t<dd class="f2"><span class="ph_num">'+config.skillPhone.split("：")[1]+'</span></dd>\n' +
         '\t\t\t</dl>\n' +
         '\t\t</div>\n' +
-        // '\t\t<div class="blank0"></div>\n' +
-        // '\t\t<div class="qqcall"> \n' +
-        // '\t\t\t<dl class="fn_cle">\n' +
-        // '\t\t\t\t<dt><img src="images/zxkfqq.png" height="31" width="31"></dt>\n' +
-        // '\t\t\t\t<dd class="f1 f_s14">在线客服：</dd>\n' +
-        // '\t\t\t\t<dd class="f2 kefuQQ">\n' +
-        // '\t\t\t\t\t<span>客服一</span>\n' +
-        // '\t\t\t\t\t<a target="_blank" href="http://wpa.qq.com/msgrd?v=3&amp;uin=1141797433&amp;site=qq&amp;menu=yes"></a>\n' +
-        // '\t\t\t\t\t<span>客服二</span>\n' +
-        // '\t\t\t\t\t<a target="_blank" href="http://yyson.com/index.html"></a>\n' +
-        // '\t\t\t\t</dd>\n' +
-        // '\t\t\t</dl>\n' +
-        // '\t\t\t<div class="blank0"></div>\n' +
-        // '\t\t</div> \n' +
-        // '\t\t<div class="blank0"></div>\n' +
-        // '\t\t<div class="weixincall"> \n' +
-        // '\t\t\t<dl class="fn_cle">\n' +
-        // '\t\t\t\t<dt><img src="images/weixin.png" height="31" width="31"></dt>\n' +
-        // '\t\t\t\t<dd class="f1">官方微信站：</dd>\n' +
-        // '\t\t\t\t<dd class="f3"><img src="" height="73" width="73"></dd>\n' +
-        // '\t\t\t</dl>\n' +
-        // '\t\t</div> \n' +
-        // '\t\t<div class="blank0"></div>\n' +
-        // '\t\t<div class="dytimer">\n' +
-        // '\t\t\t<span style="font-weight: bold;">公司地址：</span>\n' +
-        // '\t\t\t<span>四川省成都市锦江区工业园区墨香路87号8栋4楼</span>\n' +
-        // '\t\t</div>' +
         '\t\t</div>\n' +
         '\t</div>';
     $(".contactusdiyou").html(contactusdiyou);
