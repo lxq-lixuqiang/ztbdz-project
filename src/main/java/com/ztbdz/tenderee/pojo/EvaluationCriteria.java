893c522805c434bbf14bcff38a5dcf26d35a21da
lixuqiang3204 <HMV33T3D>
2025-10-13 15:58:14 +0800

    评审页面打印功能按钮，将评审页面填写的信息进行打印

--- src/main/java/com/ztbdz/tenderee/pojo/EvaluationCriteria.java
+++ src/main/java/com/ztbdz/tenderee/pojo/EvaluationCriteria.java
@@ -20,40 +20,40 @@ public class EvaluationCriteria  extends Model<EvaluationCriteria> implements Se
     private static final long serialVersionUID = 2471516643524243464L;
 
     @TableField(fill = FieldFill.INSERT)
-    @ApiModelProperty(value = "id")
+    @ApiModelProperty("id")
     private Long id;
-    @ApiModelProperty(value = "序号")
+    @ApiModelProperty("序号")
     private Integer sort;
-    @ApiModelProperty(value = "评审项名称")
+    @ApiModelProperty("评审项名称")
     private String evaluationCriteriaName;
-    @ApiModelProperty(value = "评审标准")
+    @ApiModelProperty("评审标准")
     private String evaluationCriteriaInfo;
-    @ApiModelProperty(value = "评分方式")
+    @ApiModelProperty("评分方式")
     private String evaluationWay;
-    @ApiModelProperty(value = "分数设置")
+    @ApiModelProperty("分数设置")
     private String scoreSetting;
-    @ApiModelProperty(value = "分数")
+    @ApiModelProperty("分数")
     private Integer score;
-    @ApiModelProperty(value = "是否主观项（0=主观项 1=客观项）")
+    @ApiModelProperty("是否主观项（0=主观项 1=客观项）")
     private Integer isSubjective;
-    @ApiModelProperty(value = "评标标准类型")
+    @ApiModelProperty("评标标准类型")
     private String evaluationCriteriaType;
-    @ApiModelProperty(value = "项目id")
+    @ApiModelProperty("项目id")
     private Long projectId;
-    @ApiModelProperty(value = "投标报名id")
+    @ApiModelProperty("投标报名id")
     private Long projectRegisterId;
-    @ApiModelProperty(value = "评审人员id")
+    @ApiModelProperty("评审人员id")
     @TableField(value = "member_id",el = "member.id")
     private Member member;
-    @ApiModelProperty(value = "评审类型（0=其他 1=资格评审标准 2=实质性符合标准 3=评分标准）")
+    @ApiModelProperty("评审类型（0=其他 1=资格评审标准 2=实质性符合标准 3=评分标准）")
     private Integer reviewType;
-    @ApiModelProperty(value = "是否通过（0=通过 1=不通过）")
+    @ApiModelProperty("是否通过（0=通过 1=不通过）")
     private Integer isPass;
-    @ApiModelProperty(value = "评审说明")
+    @ApiModelProperty("评审说明")
     private String reviewDescription;
-    @ApiModelProperty(value = "扣分理由")
+    @ApiModelProperty("扣分理由")
     private String deductionReason;
-    @ApiModelProperty(value = "投标报名信息")
+    @ApiModelProperty("投标报名信息")
     @TableField(exist = false)
     private ProjectRegister projectRegister;
 
